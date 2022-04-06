ad4738704697e53e375ba6eff88addd271c8958d
Zach Smith <zachsmith.sa@gmail.com>
2022-04-06 15:20:12 +0200

    Trying docker python execution via Github actions

--- post-processing/algoa-bay-forecast/src/__init__.py
+++ post-processing/algoa-bay-forecast/src/__init__.py
@@ -1,3 +1,10 @@
+import ast
+import matplotlib.pyplot as plt
+import xarray as xr
+import geojsoncontour
 import numpy as np
+from geojson import FeatureCollection
+# import cartopy.crs as ccrs
+from datetime import timedelta, datetime
 
-print("hello there")
+print("hello world. If you see this the dependencies installed correctly on deployment")
