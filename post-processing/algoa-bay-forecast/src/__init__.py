df52dad997ead0e578d94eebfb16b87e505fe5e3
mattcarr03 <Matthewcarr03@gmail.com>
2022-04-07 14:50:36 +0200

    More tests

--- post-processing/algoa-bay-forecast/src/__init__.py
+++ post-processing/algoa-bay-forecast/src/__init__.py
@@ -7,4 +7,4 @@ from geojson import FeatureCollection
 #import cartopy.crs as ccrs
 from datetime import timedelta, datetime
 
-print("Hello world. If you see this the dependencies installed correctly on deployment + test")
+print("hello world. If you see this the dependencies installed correctly on deployment + test")
