7bf77adb0fce4ff06bf09b869ccbaca83d8c0cd7
mattcarr03 <Matthewcarr03@gmail.com>
2022-04-08 15:09:25 +0200

    Added python libaries to connect to databases

--- post-processing/algoa-bay-forecast/src/__init__.py
+++ post-processing/algoa-bay-forecast/src/__init__.py
@@ -6,5 +6,29 @@ import numpy as np
 from geojson import FeatureCollection
 import cartopy.crs as ccrs
 from datetime import timedelta, datetime
+from pymongo import MongoClient
+from environs import Env
+import psycopg2
+import os
+
+# Using os to get enviromental variables (comment out for script to run, not sure variables are set)
+#USERNAME = os.environ.get("USERNAME")
+#PASSWORD = os.environ.get("PASSWORD")
+
+# Using Env to get enviromental variables (comment out for script to run, not sure variables are set)
+#env = Env()
+#USERNAME = env('USERNAME')
+#PASSWORD = env('PASSWORD')
+
+#Connect to mongo
+#client = MongoClient(username=USERNAME,
+#                 password=PASSWORD)
+
+#Conect to postgres
+#conn = psycopg2.connect(
+#    host="localhost",
+#    database="",
+#    user=USERNAME,
+#    password=PASSWORD)
 
 print("hello world. If you see this the dependencies installed correctly on deployment + test")
