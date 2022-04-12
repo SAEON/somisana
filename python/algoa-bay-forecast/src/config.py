ea0b4b6205e6c986349fc96ddfcff3d3f769a82a
Zach Smith <zachsmith.sa@gmail.com>
2022-04-12 06:40:22 +0200

    Added DB configuration for python scripts, and updated the repo layout to have a top-level python folder

--- python/algoa-bay-forecast/src/config.py
+++ python/algoa-bay-forecast/src/config.py
@@ -3,11 +3,11 @@ env = Env()
 
 PG_HOST = env.str('PG_HOST', 'localhost')
 PG_PORT = env.int('PG_PORT', 5432)
-PG_DB = 'somisana'
+PG_DB = env.str('PG_DB', 'somisana_local')
 PG_USERNAME = env.str('PG_USERNAME', 'admin')
 PG_PASSWORD = env.str('PG_PASSWORD', 'password')
 
 MONGO_HOST = env.str('MONGO_HOST', 'localhost:27017')
-MONGO_DB = 'somisana'
+MONGO_DB = env.str('MONGO_DB', 'somisana_local') 
 MONGO_USERNAME = env.str('MONGO_USERNAME', 'admin')
 MONGO_PASSWORD = env.str('MONGO_PASSWORD', 'password')
