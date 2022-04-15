4f1b9203548011d2e1ec3f601100a27ae2daea25
Zach Smith <zachsmith.sa@gmail.com>
2022-04-15 08:28:03 +0200

    Adding prettier task

--- web/server/config/_app.js
+++ web/server/config/_app.js
@@ -1,3 +1,4 @@
+export const NODE_ENV = process.env.NODE_ENV || 'development'
 export const DEPLOYMENT_ENV = process.env.DEPLOYMENT_ENV || 'development'
 export const PORT = process.env.PORT || 3000
 export const ALLOWED_ORIGINS = process.env.ALLOWED_ORIGINS || 'http://localhost:3000'
