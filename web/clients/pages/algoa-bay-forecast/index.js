233f3251d66cffe1828fb42f19ce451dc1822fa0
Zach Smith <zachsmith.sa@gmail.com>
2022-04-19 12:22:43 +0200

    SSR working example

--- web/clients/pages/algoa-bay-forecast/index.js
+++ web/clients/pages/algoa-bay-forecast/index.js
@@ -2,10 +2,10 @@ import mount from '../../modules/app-entry/main.js'
 import ConfigProvider from '../../modules/config/index.js'
 import Button from '../../components/button/index.js'
 
-const App = () => (
+export const Page = () => (
   <ConfigProvider>
     <Button>Algoa Bay</Button>
   </ConfigProvider>
 )
 
-mount(App)
+export default () => mount(Page)
