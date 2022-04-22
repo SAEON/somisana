6de7618fc8058d9d64b6859f4bf789c1c99ed200
Zach Smith <zachsmith.sa@gmail.com>
2022-04-22 11:18:42 +0200

    prettier

--- web/loaders/config.js
+++ web/loaders/config.js
@@ -1,7 +1,7 @@
 import * as importMapLoader from '@node-loader/import-maps'
 import * as httpLoader from '@node-loader/http'
-import * as geojson from './loaders/geojson.js'
+import * as geojson from './geojson.js'
 
 export default {
-  loaders: [importMapLoader, httpLoader, geojson]
+  loaders: [importMapLoader, httpLoader, geojson],
 }
