9a88f953000a1a5e8e2a03906be89b8685c84e68
Zach Smith <zachsmith.sa@gmail.com>
2022-04-29 15:19:52 +0200

    Typo

--- web/loaders/standard-lib-imports.js
+++ web/loaders/standard-lib-imports.js
@@ -1,7 +1,7 @@
 /**
  * TODO
  *
- * It would be better if the import maps ommitted the std lib
+ * It would be better if the import maps omitted the std lib
  * imports for node, or if the imports then worked
  */
 export async function resolve(specifier, ctx, defaultResolve) {
