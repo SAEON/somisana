911b545c4a974326c7a3bccd705b3cccb704c122
Zach Smith <zachsmith.sa@gmail.com>
2022-05-06 16:31:19 +0200

    Smoother home page loading

--- web/client/pages/somisana/routes/home/index.tsx
+++ web/client/pages/somisana/routes/home/index.tsx
@@ -1,41 +1,44 @@
 import Grid from '@mui/material/Grid'
 import Typography from '@mui/material/Typography'
 import { alpha } from '@mui/material/styles'
+import Fade from '@mui/material/Fade'
 
 const Home = () => {
   return (
-    <Grid
-      sx={{
-        height: '100vh',
-        alignContent: 'center',
-        justifyContent: 'center',
-      }}
-      container
-    >
-      <Grid item xs={12} sm={12} md={8} xl={6}>
-        <Typography
-          sx={{
-            textAlign: 'center',
-            color: theme => alpha(theme.palette.primary.main, 0.9),
-          }}
-          variant="h1"
-        >
-          SOMISANA
-        </Typography>
-        <Typography
-          sx={{
-            textAlign: 'center',
-            color: theme => alpha(theme.palette.primary.main, 0.9),
-          }}
-          variant="h5"
-          variantMapping={{
-            h5: 'h2',
-          }}
-        >
-          Sustainable Ocean Modelling Initiative: A South African Approach
-        </Typography>
+    <Fade timeout={2500} key="home" in>
+      <Grid
+        sx={{
+          height: '100vh',
+          alignContent: 'center',
+          justifyContent: 'center',
+        }}
+        container
+      >
+        <Grid item xs={12} sm={12} md={8} xl={6}>
+          <Typography
+            sx={{
+              textAlign: 'center',
+              color: theme => alpha(theme.palette.primary.main, 0.9),
+            }}
+            variant="h1"
+          >
+            SOMISANA
+          </Typography>
+          <Typography
+            sx={{
+              textAlign: 'center',
+              color: theme => alpha(theme.palette.primary.main, 0.9),
+            }}
+            variant="h5"
+            variantMapping={{
+              h5: 'h2',
+            }}
+          >
+            Sustainable Ocean Modelling Initiative: A South African Approach
+          </Typography>
+        </Grid>
       </Grid>
-    </Grid>
+    </Fade>
   )
 }
 
