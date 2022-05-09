014cd72c318a2c733d58c0052b6948c03d510a14
Zach Smith <zachsmith.sa@gmail.com>
2022-05-09 12:03:38 +0200

    Started incorporating site-wide settings into the web client - cookies, language to start.

--- web/client/components/icons/index.tsx
+++ web/client/components/icons/index.tsx
@@ -1,8 +1,26 @@
 import SvgIcon from '@mui/material/SvgIcon'
-import { mdiCog } from '@mdi/js'
+import { mdiCog, mdiClose, mdiChevronDown, mdiContentSaveAll } from '@mdi/js'
 
-export const Cog = (props: object) => (
+export const Cog = (props: any) => (
   <SvgIcon {...props}>
     <path d={mdiCog} />
+  </SvgIcon>
+)
+
+export const Close = (props: any) => (
+  <SvgIcon {...props}>
+    <path d={mdiClose} />
+  </SvgIcon>
+)
+
+export const ExpandMore = (props: any) => (
+  <SvgIcon {...props}>
+    <path d={mdiChevronDown} />
+  </SvgIcon>
+)
+
+export const SaveAll = (props: any) => (
+  <SvgIcon {...props}>
+    <path d={mdiContentSaveAll} />
   </SvgIcon>
 )
--- web/client/pages/somisana/routes/home/index.tsx
+++ web/client/pages/somisana/routes/home/index.tsx
@@ -1,20 +1,11 @@
-import Tooltip from '@mui/material/Tooltip'
-import IconButton from '@mui/material/IconButton'
-import { Cog } from '../../../../components/icons'
 import Title from './title'
+import SiteSettings from './site-settings'
 
 const Home = () => {
   return (
     <>
       <Title />
-      <Tooltip placement="left" title="Site settings">
-        <IconButton
-          size="medium"
-          sx={{ position: 'absolute', bottom: 0, right: 0, margin: theme => theme.spacing(2) }}
-        >
-          <Cog />
-        </IconButton>
-      </Tooltip>
+      <SiteSettings />
     </>
   )
 }
