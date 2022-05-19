b3b7ca1326dcd7a671c4d7f1a50236bdb119b8f2
Zach Smith <zachsmith.sa@gmail.com>
2022-05-19 12:04:22 +0200

    Added header/footer to site settings menu

--- web/client/pages/somisana/routes/home/site-settings/index.tsx
+++ web/client/pages/somisana/routes/home/site-settings/index.tsx
@@ -11,6 +11,7 @@ import { Cog, Close, CheckAll } from '../../../../../components/icons'
 import LanguageSettings from './language'
 import ThemeSettings from './theme'
 import CookieSettings from './cookies'
+import AppBar from '@mui/material/AppBar'
 
 const SiteSettingsPanel = () => {
   const { updateSetting, ...settings } = useContext(siteSettingsContext)
@@ -37,6 +38,9 @@ const SiteSettingsPanel = () => {
 
       {/* DRAWER */}
       <SwipeableDrawer
+        sx={{
+          display: 'flex',
+        }}
         variant="temporary"
         ModalProps={{
           keepMounted: true,
@@ -46,23 +50,25 @@ const SiteSettingsPanel = () => {
         onClose={() => setOpen(false)}
         onOpen={() => setOpen(true)}
       >
-        <Toolbar
-          disableGutters
-          sx={{
-            padding: theme => theme.spacing(1),
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'space-between',
-          }}
-        >
-          <Typography>Site settings</Typography>
-          <IconButton onClick={() => setOpen(false)} size="small">
-            <Close />
-          </IconButton>
-        </Toolbar>
+        <AppBar elevation={0} variant="outlined" position="relative">
+          <Toolbar
+            disableGutters
+            sx={{
+              padding: theme => theme.spacing(1),
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+            }}
+          >
+            <Typography>Site settings</Typography>
+            <IconButton onClick={() => setOpen(false)} size="small">
+              <Close />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
         <Box
           sx={theme => ({
-            minWidth: 400,
+            overflow: 'auto',
             [theme.breakpoints.up('sm')]: {
               maxWidth: 400,
             },
@@ -72,30 +78,32 @@ const SiteSettingsPanel = () => {
           <CookieSettings />
           <ThemeSettings />
         </Box>
-        <Toolbar
-          disableGutters
-          variant="dense"
-          sx={{
-            padding: theme => theme.spacing(1),
-            display: 'flex',
-            alignItems: 'center',
-            flexDirection: 'row-reverse',
-            marginTop: 'auto',
-          }}
-        >
-          <Button
-            color="primary"
-            onClick={() => {
-              setOpen(false)
-              updateSetting({ accepted: true })
+        <AppBar elevation={0} variant="outlined" position="relative" sx={{ marginTop: 'auto' }}>
+          <Toolbar
+            disableGutters
+            variant="dense"
+            sx={{
+              padding: theme => theme.spacing(1),
+              display: 'flex',
+              alignItems: 'center',
+              flexDirection: 'row-reverse',
+              marginTop: 'auto',
             }}
-            variant="contained"
-            size="medium"
-            startIcon={<CheckAll />}
           >
-            Accept
-          </Button>
-        </Toolbar>
+            <Button
+              color="primary"
+              onClick={() => {
+                setOpen(false)
+                updateSetting({ accepted: true })
+              }}
+              variant="contained"
+              size="medium"
+              startIcon={<CheckAll />}
+            >
+              Accept
+            </Button>
+          </Toolbar>
+        </AppBar>
       </SwipeableDrawer>
     </>
   )
