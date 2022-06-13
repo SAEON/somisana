6952a9a3345bcd0ac3f37f40e302f49b27d5dd1a
Zach Smith <zachsmith.sa@gmail.com>
2022-06-13 10:01:13 +0200

    Update _content.ts
    Testing editor

--- web/client/modules/about/_content.ts
+++ web/client/modules/about/_content.ts
@@ -1,4 +1,10 @@
 export default `
+# Another heading
+Some content
+
+## Sub heading
+Sub heading content
+
 # Our vision
 Facilitate the local development and sustainability of an operational ocean current forecast system for the South African exclusive economic zone and to do so in a transformative fashion
 `
