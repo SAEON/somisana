c90e1f186ec267d74652ccfdbf193c1d52a4a588
Zach Smith <zachsmith.sa@gmail.com>
2022-06-28 11:16:34 +0200

    Added full model output .nc files for post processing local testing

--- models/algoa-bay-forecast/toolkit/post-processing/src/config/__init__.py
+++ models/algoa-bay-forecast/toolkit/post-processing/src/config/__init__.py
@@ -12,5 +12,5 @@ MONGO_DB = env.str('MONGO_DB', 'somisana_local')
 MONGO_USERNAME = env.str('MONGO_USERNAME', 'admin')
 MONGO_PASSWORD = env.str('MONGO_PASSWORD', 'password')
 
-MODEL_OUTPUT_PATH = env.str('MODEL_OUTPUT_PATH', './.model-out/avg_surf_20210115.nc')
+MODEL_OUTPUT_PATH = env.str('MODEL_OUTPUT_PATH', './.model-out/avg.nc')
 MODEL_GRID_PATH = env.str('MODEL_GRID_PATH', './.model-out/grid.nc')
