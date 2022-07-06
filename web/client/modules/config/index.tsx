a3d58d596604b8873bd730cc8a605deeab5d1f8c
Zach Smith <zachsmith.sa@gmail.com>
2022-07-06 11:33:13 +0200

    Refactoring

--- web/client/modules/config/index.tsx
+++ web/client/modules/config/index.tsx
@@ -1,31 +1,30 @@
 import { createContext, useState } from 'react'
 import * as _config from './env/index.js'
 
-if (_config.NODE_ENV !== 'production') {
-  const ignore: string[] = []
-
-  console.info(
-    'Client configuration',
-    Object.fromEntries(
-      Object.entries(_config)
-        .filter(([field]) => !ignore.includes(field))
-        .map(([field, value]: [string, any]) => [
-          field,
-          typeof value === 'function' ? value.toString() : value,
-        ])
-        .sort(([aKey], [bKey]) => {
-          if (aKey > bKey) return 1
-          if (bKey > aKey) return -1
-          return 0
-        })
-    )
-  )
-}
-
 export const ctx = createContext(_config)
 
 const ConfigProvider = props => {
   const [config, setConfig] = useState(_config)
+
+  if (config.NODE_ENV !== 'production') {
+    const ignore: string[] = []
+    console.info(
+      'Client configuration',
+      Object.fromEntries(
+        Object.entries(config)
+          .filter(([field]) => !ignore.includes(field))
+          .map(([field, value]: [string, any]) => [
+            field,
+            typeof value === 'function' ? value.toString() : value,
+          ])
+          .sort(([aKey], [bKey]) => {
+            if (aKey > bKey) return 1
+            if (bKey > aKey) return -1
+            return 0
+          })
+      )
+    )
+  }
 
   return <ctx.Provider value={{ ...config, setConfig }} {...props} />
 }
