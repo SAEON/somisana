bbfd704bacb714248ba61247c3f4fc6360377de1
Zach Smith <zachsmith.sa@gmail.com>
2022-07-11 11:12:48 +0200

    added backdrop color for atlas to improve the page when loading

--- web/client/pages/atlas/ssr.tsx
+++ web/client/pages/atlas/ssr.tsx
@@ -3,7 +3,13 @@ import Map from './map'
 
 export default () => {
   return (
-    <Box sx={{ flexGrow: 1 }}>
+    <Box
+      sx={{
+        flexGrow: 1,
+        background: theme =>
+          `linear-gradient(to top, ${theme.palette.primary.light}, ${theme.palette.grey[100]}, ${theme.palette.common.white})`,
+      }}
+    >
       <Map />
     </Box>
   )
