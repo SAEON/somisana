f6ace0fddaa85140a15d84a3e1d4d4ab10c237a4
Zach Smith <zachsmith.sa@gmail.com>
2022-07-23 07:34:55 +0200

    Removed SQL from false bay post processing as it was inserting into algoa bay table

--- models/false-bay-forecast/toolkit/post-processing/src/postgis/sql/wms-abf-salinity.sql
+++
@@ -1,4 +0,0 @@
-create materialized view if not exists wms_abf_salinity as
-  select
-    *
-  from salinities;
--- models/false-bay-forecast/toolkit/post-processing/src/postgis/sql/wms-abf-temperature.sql
+++
@@ -1,4 +0,0 @@
-create materialized view if not exists wms_abf_temperature as
-  select
-    *
-  from temperatures;
