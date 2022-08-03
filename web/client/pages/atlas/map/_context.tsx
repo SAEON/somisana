dab92acc6d3adc093028dad53cd3465e566dbdc1
Zach Smith <zachsmith.sa@gmail.com>
2022-08-03 11:01:36 +0200

    POC esri display

--- web/client/pages/atlas/map/_context.tsx
+++ web/client/pages/atlas/map/_context.tsx
@@ -1,5 +1,8 @@
 import { createContext, useEffect, useRef, useContext, useState } from 'react'
-import MapView from '@arcgis/core/views/MapView'
+import SceneView from '@arcgis/core/views/SceneView'
+import Map from '@arcgis/core/Map'
+import esriConfig from '@arcgis/core/config'
+import VectorTileLayer from '@arcgis/core/layers/VectorTileLayer'
 import { ctx as configContext } from '../../../modules/config'
 import useTheme from '@mui/material/styles/useTheme'
 import Div from '../../../components/div'
@@ -7,8 +10,6 @@ import Span from '../../../components/span'
 import maplibre from 'maplibre-gl'
 import Typography from '@mui/material/Typography'
 import { alpha } from '@mui/system/colorManipulator'
-
-console.log(MapView)
 
 const Attribution = () => {
   return (
@@ -44,77 +45,116 @@ export default ({ children }) => {
   const theme = useTheme()
   const ref = useRef(null)
   const mapRef = useRef(null)
+  const ESRI_BASEMAP = 'arcgis-terrain'
 
   useEffect(() => {
-    const basemapEnum = 'ArcGIS:Terrain'
+    esriConfig.apiKey = ESRI_API_KEY
 
-    const map = new maplibre.Map({
-      container: ref.current,
-      style: `https://basemaps-api.arcgis.com/arcgis/rest/services/styles/${basemapEnum}?type=style&token=${ESRI_API_KEY}`,
-      center: [25, -31],
-      zoom: 5.5,
-      attributionControl: false,
+    const metadata = new VectorTileLayer({
+      style: {
+        id: 'metadata',
+        version: 8,
+        sources: {
+          models: {
+            type: 'vector',
+            tiles: ['http://localhost:7800/public.metadata/{z}/{x}/{y}.pbf'],
+          },
+        },
+        layers: [
+          {
+            id: 'model-metadata',
+            type: 'fill',
+            source: 'models',
+            minzoom: 0,
+            maxzoom: 24,
+            'source-layer': 'public.metadata',
+            paint: {
+              'fill-color': theme.palette.primary.dark,
+              // 'fill-opacity': ['case', ['boolean', ['feature-state', 'hover'], false], 0.6, 0.2],
+              'fill-opacity': 0.2,
+              'fill-outline-color': theme.palette.primary.dark,
+            },
+          },
+        ],
+      },
     })
+
+    const map = new Map({
+      basemap: ESRI_BASEMAP,
+    })
+
+    const view = new SceneView({
+      map,
+      center: [25, -31],
+      zoom: 6,
+      container: ref.current,
+    })
+
+    setTimeout(() => {
+      map.add(metadata)
+      console.log('working')
+    }, 3000)
 
     if (NODE_ENV !== 'production') {
       window.map = map
+      window.view = view
     }
 
-    map.on('load', () => {
-      map.addSource('metadata', {
-        type: 'vector',
-        tiles: [`${TILESERV_BASE_URL}/public.metadata/{z}/{x}/{y}.pbf`],
-        url: `${TILESERV_BASE_URL}/public.metadata.json`,
-        promoteId: 'id',
-      })
+    // map.on('load', () => {
+    //   map.addSource('metadata', {
+    //     type: 'vector',
+    //     tiles: [`${TILESERV_BASE_URL}/public.metadata/{z}/{x}/{y}.pbf`],
+    //     url: `${TILESERV_BASE_URL}/public.metadata.json`,
+    //     promoteId: 'id',
+    //   })
 
-      map.addLayer({
-        id: 'models',
-        type: 'fill',
-        source: 'metadata',
-        'source-layer': 'public.metadata',
-        paint: {
-          'fill-color': theme.palette.primary.dark,
-          'fill-opacity': ['case', ['boolean', ['feature-state', 'hover'], false], 0.6, 0.2],
-          'fill-outline-color': theme.palette.primary.dark,
-        },
-      })
+    //   map.addLayer({
+    //     id: 'models',
+    //     type: 'fill',
+    //     source: 'metadata',
+    //     'source-layer': 'public.metadata',
+    // paint: {
+    //   'fill-color': theme.palette.primary.dark,
+    //   'fill-opacity': ['case', ['boolean', ['feature-state', 'hover'], false], 0.6, 0.2],
+    //   'fill-outline-color': theme.palette.primary.dark,
+    // },
+    //   })
 
-      let featureHoveredId = null
-      map.on('mouseenter', 'models', ({ features }) => {
-        featureHoveredId = features[0].id
-        map.getCanvas().style.cursor = 'pointer'
-        map.setFeatureState(
-          { source: 'metadata', id: featureHoveredId, sourceLayer: 'public.metadata' },
-          { hover: true }
-        )
-      })
+    //   let featureHoveredId = null
+    //   map.on('mouseenter', 'models', ({ features }) => {
+    //     featureHoveredId = features[0].id
+    //     map.getCanvas().style.cursor = 'pointer'
+    //     map.setFeatureState(
+    //       { source: 'metadata', id: featureHoveredId, sourceLayer: 'public.metadata' },
+    //       { hover: true }
+    //     )
+    //   })
 
-      map.on('mouseleave', 'models', () => {
-        map.getCanvas().style.cursor = ''
-        map.setFeatureState(
-          { source: 'metadata', id: featureHoveredId, sourceLayer: 'public.metadata' },
-          { hover: false }
-        )
-      })
+    //   map.on('mouseleave', 'models', () => {
+    //     map.getCanvas().style.cursor = ''
+    //     map.setFeatureState(
+    //       { source: 'metadata', id: featureHoveredId, sourceLayer: 'public.metadata' },
+    //       { hover: false }
+    //     )
+    //   })
 
-      map.on('click', 'models', ({ features: [feature] }) => {
-        const { min_x, min_y, max_x, max_y } = feature.properties
-        const center = [min_x + (max_x - min_x) / 2, min_y + (max_y - min_y) / 2]
-        map.fitBounds(
-          [
-            [min_x, max_y],
-            [max_x, min_y],
-          ],
-          {
-            linear: false,
-            padding: 48,
-            curve: 1,
-            speed: 1.5,
-          }
-        )
-      })
-    })
+    //   map.on('click', 'models', ({ features: [feature] }) => {
+    //     const { min_x, min_y, max_x, max_y } = feature.properties
+    //     const center = [min_x + (max_x - min_x) / 2, min_y + (max_y - min_y) / 2]
+    //     map.fitBounds(
+    //       [
+    //         [min_x, max_y],
+    //         [max_x, min_y],
+    //       ],
+    //       {
+    //         linear: false,
+    //         padding: 48,
+    //         curve: 1,
+    //         speed: 1.5,
+    //       }
+    //     )
+    //   })
+    // })
   }, [])
 
   return (
