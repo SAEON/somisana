0d327922fef89106135eaf27a78907dc9c1d9536
Zach Smith <zachsmith.sa@gmail.com>
2022-08-16 06:44:44 +0200

    Updated deps and pushed fixed for resolving node interface to locales

--- web/server/graphql/resolvers/types/node/index.js
+++ web/server/graphql/resolvers/types/node/index.js
@@ -1,10 +1,23 @@
-export default {
-  __resolveType: ({ _gqlType }) => {
-    if (!_gqlType) {
+import collections from '../../../../mongo/collections/index.js'
+
+const types = Object.fromEntries(
+  Object.entries(collections).map(([, { name, gqlType }]) => {
+    if (!gqlType) {
       throw new Error(
-        `Resolves must append a "_gqlType" field to every object so that the concrete type can be worked out when resolving objects that implement the Node interface`
+        'All collections defined in ..../mongo/collections/index.js must have the field "gqlType"'
       )
     }
-    return _gqlType
+    return [name, gqlType]
+  })
+)
+
+export default {
+  __resolveType: ({ _gqlType, __collection: collection }) => {
+    if (!(_gqlType || collection)) {
+      throw new Error(
+        `Resolves must append a "_gqlType" or "__collection" field to every object so that the concrete type can be worked out when resolving objects that implement the Node interface`
+      )
+    }
+    return _gqlType || types[collection]
   },
 }
