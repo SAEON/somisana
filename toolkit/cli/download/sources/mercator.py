54339c23e9a619a42eef624398aefd8e7ecc3f44
Zach Smith <zachsmith.sa@gmail.com>
2022-08-20 06:59:54 +0200

    Fix for mercator download to error on failure so that restarts can be triggered (#6)

--- toolkit/cli/download/sources/mercator.py
+++ toolkit/cli/download/sources/mercator.py
@@ -41,19 +41,12 @@ def mercator(usrname, passwd, domain, date_now, hdays, fdays, varList, depths, d
         ' --out-dir ' + os.path.normpath(dirout) + ' --out-name ' + fname
 
     if os.path.exists(os.path.normpath(os.path.join(dirout, fname))) == False:
-        # run the runcommand, i.e. download the data specified above
         print('downloading latest mercator ocean forecast from CMEMS...')
         startTime = datetime.now()
-        try:
-            os.system(runcommand)
-            print('mercator download completed (in ' +
-                  str(datetime.now() - startTime)+' h:m:s)')
-        except:
-            # for now we'll just terminate today's forecast if the mercator download fails
-            sys.exit('mercator data not available for today- forecast not executed')
-            # an alternative approach might be to return a flag from this function to say whether the file was downloaded or not
-            # then in run_frcst.py we could use that flag to either make a new croco file from the downloaded mercator file
-            # or if the file wasn't downloaded then we could use yesterday's croco file and just repeat the last available time-step to make a new one
+        if os.system(runcommand) != 0:
+            raise Exception('Mercator download failed from cmd: ' + runcommand)
+        else:
+            print('mercator download completed (in ' + str(datetime.now() - startTime) + ' h:m:s)')
     else:
         print(os.path.normpath(os.path.join(dirout, fname)) +
               ' already exists - not downloading mercator data')
