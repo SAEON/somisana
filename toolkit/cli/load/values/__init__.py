4613e91a52c497cb547d71e8bef84f86503b3dba
Zach Smith <zachsmith.sa@gmail.com>
2022-08-22 17:05:26 +0200

    Moved the scheduled algoa bay run earlier to allow for all the postgres processing

--- toolkit/cli/load/values/__init__.py
+++ toolkit/cli/load/values/__init__.py
@@ -10,7 +10,9 @@ def upsert(model, run_date, now):
   # Upsert for every depth at every time step
   for time_step in range(240):
     for depth_level in range(20):
-      print('--> Refreshing values at depth level', depth_level + 1, 'timestep', time_step + 1, str(datetime.now() - now))
+      d = depth_level + 1
+      t = time_step + 1
+      print('--> Refreshing values at depth level', f'{d:02}', 'timestep', f'{t:03}', str(datetime.now() - now))
       cursor = connect().cursor()
       cursor.execute("""
-        select upsert_values (modelid => %s, rundate => %s,  depth_level => %s, time_step => %s)""", (modelid, run_date, depth_level + 1, time_step + 1))
+        select upsert_values (modelid => %s, rundate => %s,  depth_level => %s, time_step => %s)""", (modelid, run_date, d, t))
