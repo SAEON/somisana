bc8eeb2acbfed09a06cb1eea6eb73c66a4ee7488
Zach Smith <zachsmith.sa@gmail.com>
2022-08-25 07:42:55 +0200

    Further refactoring gfs download script towards accepting a date param (#6)

--- toolkit/cli/download/sources/gfs.py
+++ toolkit/cli/download/sources/gfs.py
@@ -1,8 +1,6 @@
 import requests as r
 from datetime import datetime, timedelta, time
-import sys
-import os
-from cli.download.sources.gfs_functions import yyyymmdd, time_param, create_fname, validate_download_or_remove, make_cURL
+from cli.download.sources.gfs_functions import yyyymmdd, download_file, get_latest_available_dt
 
 """
 Download GFS forecast data for running a croco model
@@ -15,15 +13,17 @@ The latest initialisation is used at the time this is run, so that if there is a
 side then a slightly older initialization will be found
 """
 
-BASE_URL = 'https://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_0p25_1hr.pl?file=gfs.t'
-
 def gfs(date_now, hdays, fdays, domain, dirout):
-    start_time = datetime.now()
-    date_now = datetime.combine(date_now, time())
+    now = datetime.now()
     hdays = hdays + 0.25
     fdays = fdays + 0.25
+    date_now = datetime.combine(date_now, time())
+    start_date = date_now + timedelta(days =- hdays)
 
-    URL_PARAMS = '&lev_10_m_above_ground=on&lev_2_m_above_ground=on&lev_surface=on&var_DLWRF=on&var_DSWRF=on&var_LAND' \
+    latest_available_date = get_latest_available_dt(date_now)
+    delta_days = (latest_available_date - date_now).total_seconds() / 86400  
+    
+    params = '&lev_10_m_above_ground=on&lev_2_m_above_ground=on&lev_surface=on&var_DLWRF=on&var_DSWRF=on&var_LAND' \
         + '=on&var_PRATE=on&var_RH=on&var_TMP=on&var_UFLX=on&var_UGRD=on&var_ULWRF=on&var_USWRF=on&var_VFLX=on&var_VGRD=on&' \
         + 'subregion=&leftlon=' \
         + str(domain[0]) \
@@ -34,86 +34,17 @@ def gfs(date_now, hdays, fdays, domain, dirout):
         + '&bottomlat=' \
         + str(domain[2]) \
         + '&dir=/gfs.'
-    
-    date_latest = datetime(date_now.year, date_now.month, date_now.day, 18, 0, 0)
-    gfs_exists = False
-    iters = 0
 
-    while not (gfs_exists):
-        if iters > 4:
-            print("GFS data is not presently available")
-            sys.exit(1)
+    # Download forcing files up to latest available date
+    while start_date < latest_available_date:
+        for i in range(1, 7): # hours 1 to 6
+            download_file(start_date, i, dirout, params)
+        start_date = start_date + timedelta(hours=6)
 
-        url_check = \
-            'https://nomads.ncep.noaa.gov/dods/gfs_0p25_1hr/gfs' \
-            + yyyymmdd(date_latest) \
-            + '/gfs_0p25_1hr_' \
-            + date_latest.strftime("%H") \
-            + 'z'
-
-        print('Testing GFS availability', url_check)
-        result = r.head(url_check)
-        xdap = result.headers.get('XDAP')
-        if xdap:
-            print("Latest available GFS initialisation found at",url_check, '\n', 'X-DAP HTTP Header', xdap, '\n', '\n')
-            gfs_exists = True
-        else:
-            date_latest = date_latest + timedelta(hours=- 6)
-            iters += 1
-
-    print("GFS download started...")
-    delta_days = (date_latest-date_now).total_seconds() / 86400
-    date_hist = date_now + timedelta(days =- hdays)
-
-    while date_hist < date_latest:
-
-        # forecast hours 1 to 6
-        for i in range(1, 7):
-            fname = create_fname(date_hist, i)
-            fileout = os.path.join(dirout, fname)
-
-            # only download if the file doesn't already exist
-            if not (os.path.isfile(fileout)):
-                url = BASE_URL \
-                    + date_hist.strftime("%H") \
-                    + 'z.pgrb2.0p25.f' \
-                    + str(i).zfill(3) \
-                    + URL_PARAMS \
-                    + time_param(date_hist)
-
-                cmd = make_cURL(url, fileout)
-                print(cmd)
-
-                if os.system(cmd) != 0:
-                    raise Exception('GFS download failed (via cURL). ' + fname + ' could not be downloaded. ' + cmd)
-                else:
-                    validate_download_or_remove(fileout)
-
-        date_hist = date_hist + timedelta(hours=6)
-
-    # now download the forecast from date_latest, already identified as the latest initialisation of gfs
-    fhours = int( (fdays-delta_days) * 24 )
-
+    # Download forecast forcing files
+    fhours = int( (fdays - delta_days) * 24 )
     for i in range(1, fhours + 1):
-        fname = create_fname(date_latest, i)
-        fileout = os.path.join(dirout, fname)
+        download_file(latest_available_date, i, dirout, params)
 
-        # only download if the file doesn't already exist
-        if not (os.path.isfile(fileout)):
-            url = BASE_URL + \
-                date_latest.strftime("%H") \
-                + 'z.pgrb2.0p25.f' \
-                + str(i).zfill(3) \
-                + URL_PARAMS \
-                + time_param(date_latest)
-
-            cmd = make_cURL(url, fileout)
-            print(cmd)
-
-            if os.system(cmd) != 0:
-                raise Exception('GFS download failed (via cURL). ' + fname + ' could not be downloaded. ' + cmd)
-            else:
-                validate_download_or_remove(fileout)
-
-    print('GFS download completed (in '+str(datetime.now() - start_time)+' h:m:s)')
+    print('GFS download completed (in '+str(datetime.now() - now)+' h:m:s)')
     return delta_days 
--- toolkit/cli/download/sources/gfs_functions.py
+++ toolkit/cli/download/sources/gfs_functions.py
@@ -1,16 +1,23 @@
 import os
 from pathlib import Path
+from datetime import datetime, timedelta
+import requests as r
+
+BASE_URL = 'https://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_0p25_1hr.pl?file=gfs.t'
+
 
 def yyyymmdd(dt):
     return dt.strftime("%Y") \
         + dt.strftime("%m") \
         + dt.strftime("%d")
 
+
 def time_param(dt):
     return yyyymmdd(dt) \
         + '/' \
         + dt.strftime("%H") \
         + '/atmos'
+
 
 def create_fname(dt, i):
     return yyyymmdd(dt) \
@@ -19,10 +26,13 @@ def create_fname(dt, i):
         + str(i).zfill(3) \
         + '.grb'
 
+
 def validate_download_or_remove(fileout):
-    if Path(fileout).stat().st_size < 1000:  
-        print('WARNING:', fileout, '< 1kB (flagged as invalid)', open(fileout, 'r').read())
+    if Path(fileout).stat().st_size < 1000:
+        print('WARNING:', fileout, '< 1kB (flagged as invalid)',
+              open(fileout, 'r').read())
         os.remove(fileout)
+
 
 def make_cURL(url, fileout):
     return 'curl --silent \'' \
@@ -30,3 +40,62 @@ def make_cURL(url, fileout):
         + '\'' \
         + ' -o ' \
         + fileout
+
+
+def make_url(dt, i, URL_PARAMS):
+    return BASE_URL \
+        + dt.strftime("%H") \
+        + 'z.pgrb2.0p25.f' \
+        + str(i).zfill(3) \
+        + URL_PARAMS \
+        + time_param(dt)
+
+
+def exec_cmd(cmd, fileout):
+    if os.system(cmd) != 0:
+        raise Exception('GFS download failed from command: ' + cmd)
+    else:
+        validate_download_or_remove(fileout)
+
+
+def download_file(dt, i, dirout, PARAMS):
+    fname = create_fname(dt, i)
+    fileout = os.path.join(dirout, fname)
+
+    if not (os.path.isfile(fileout)):
+        url = make_url(dt, i, PARAMS)
+        cmd = make_cURL(url, fileout)
+        print(cmd)
+        exec_cmd(cmd, fileout)
+    else:
+        print('File already exists', fileout)
+
+def get_latest_available_dt(dt):
+    latest_available_date = datetime(dt.year, dt.month, dt.day, 18, 0, 0)
+    gfs_exists = False
+    iters = 0
+
+    while not (gfs_exists):
+        if iters > 4:
+            print("GFS data is not presently available")
+            exit(1)
+
+        dataset_url = \
+            'https://nomads.ncep.noaa.gov/dods/gfs_0p25_1hr/gfs' \
+            + yyyymmdd(latest_available_date) \
+            + '/gfs_0p25_1hr_' \
+            + latest_available_date.strftime("%H") \
+            + 'z'
+
+        print('Testing GFS availability', dataset_url)
+        result = r.head(dataset_url)
+        xdap = result.headers.get('XDAP')
+
+        if xdap:
+            print("Latest available GFS initialisation found at",dataset_url, '\n', 'X-DAP HTTP Header', xdap, '\n', '\n')
+            gfs_exists = True
+        else:
+            latest_available_date = latest_available_date + timedelta(hours=- 6)
+            iters += 1
+    
+    return latest_available_date
