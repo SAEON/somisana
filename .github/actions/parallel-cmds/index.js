5a9c80f689dbc3bf8863f6acb89e1bed3e2951ec
Zach Smith <zachsmith.sa@gmail.com>
2022-09-06 11:09:04 +0200

    WIP - testing local parallel-cmds action

--- .github/actions/parallel-cmds/index.js
+++ .github/actions/parallel-cmds/index.js
@@ -5,25 +5,34 @@ import {cpus} from 'os'
 import {spawn } from 'child_process'
 
 try {
+  
   const cores = core.getInput('cores') || cpus().length
-  const commands = core.getInput('cmds')
+  const commands = core.getInput('cmds').split('\n').map(s => s.split(' ').map(s => s.trim()).flat())
+
+  console.log(commands)
 
   console.log('cores', cores)
 
-  const results = await Promise.all(commands.split('\n').map(s =>  new Promise((resolve, reject) => {
-      const [cmd, ...args] = s.trim().split(' ').map(c => c.trim())
-      console.info('Executing cmd', cmd)
-      const p = spawn(cmd, args)
-      p.on('message', msg => console.info(msg.toString()))
-      p.on('exit', code => {
-        if (code === 0) {
-          resolve()
-        } else {
-          reject()
-        }
-      })
-    })
-  ))
+  let usedCores = 0
+  while (usedCores < cores) {
+
+    usedCores ++
+  }
+
+  // const results = await Promise.all(commands.split('\n').map(s =>  new Promise((resolve, reject) => {
+  //     const [cmd, ...args] = s.trim().split(' ').map(c => c.trim())
+  //     console.info('Executing cmd', cmd, args)
+  //     const p = spawn(cmd, args)
+  //     p.on('message', msg => console.info(msg.toString()))
+  //     p.on('exit', code => {
+  //       if (code === 0) {
+  //         resolve()
+  //       } else {
+  //         reject()
+  //       }
+  //     })
+  //   })
+  // ))
 
   console.log('All done!')
 
