8388c492f4cfcdea125aff54f8f13e7f6f183427
Zach Smith <zachsmith.sa@gmail.com>
2022-09-06 10:32:34 +0200

    Updated Algoa Bay Forecast run to use a bespoke action for running the load postgis commands in parallel. Previously lots of docker containers were started, but this is not ideal because (A) it seemed to fail for unkonwn reasons and (B) it makes the workflow file much longer

---
+++ .github/actions/parallel-cmds/index.js
@@ -0,0 +1,55 @@
+// https://docs.github.com/en/actions/creating-actions/creating-a-javascript-action
+import core from '@actions/core'
+import { cpus } from 'os'
+import { spawn } from 'child_process'
+
+try {
+  let usedCores = 0
+  const cores = core.getInput('cores') || cpus().length
+  const commands = core.getInput('cmds')
+  console.log( commands.split('-'))
+
+  // let commands = core
+  //   .getInput('cmds')
+  //   .split('\n')
+  //   .map(s =>
+  //     s
+  //       .split(' ')
+  //       .map(s => s.trim())
+  //       .flat()
+  //   )
+
+  // const executingCommands = []
+
+  // while (commands.length > 0) {
+  //   if (usedCores < cores) {
+  //     const [cmd, ...args] = commands.shift()
+  //     console.info('Executing cmd', cmd, args)
+  //     const promise = new Promise((resolve, reject) => {
+  //       const p = spawn(cmd, args)
+  //       p.on('message', msg => console.info(msg.toString()))
+  //       p.on('exit', c => {
+  //         if (c === 0) {
+  //           usedCores--
+  //           resolve('complete')
+  //         } else {
+  //           reject()
+  //         }
+  //       })
+  //     })
+  //     executingCommands.push(promise)
+  //     usedCores++
+  //   } else {
+  //     await new Promise(res => setTimeout(res, 1000))
+  //   }
+  // }
+
+  // await Promise.all(executingCommands)
+
+  const time = new Date().toTimeString()
+  core.setOutput('time', time)
+  console.info('All done!', time)
+} catch (error) {
+  console.error(error.message)
+  core.setFailed(error.message)
+}
