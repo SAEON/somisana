19073dfc91280962abf3a1198f6849efbae58a8a
Zach Smith <zachsmith.sa@gmail.com>
2022-09-06 10:52:30 +0200

    WIP - testing local parallel-cmds action

--- .github/actions/parallel-cmds/index.js
+++ .github/actions/parallel-cmds/index.js
@@ -1,10 +1,15 @@
 // https://docs.github.com/en/actions/creating-actions/creating-a-javascript-action
 import core from '@actions/core'
 import github from '@actions/github'
+import {cpus} from 'os'
 
 try {
+  const cores = core.getInput('cores') || cpus().length
   const commands = core.getInput('cmds')
+
+  console.log('cores', cores)
   console.log(typeof commands, commands)
+  console.log(commands.split('\n'))
 
   const time = new Date().toTimeString()
   core.setOutput('time', time)
