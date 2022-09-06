d8b5f3bc6bdf8b020e0f934f11a5e9c67bb2e144
Zach Smith <zachsmith.sa@gmail.com>
2022-09-06 12:26:37 +0200

    WIP - testing local parallel-cmds action

--- .github/actions/parallel-cmds/index.js
+++ .github/actions/parallel-cmds/index.js
@@ -6,43 +6,44 @@ import { spawn } from 'child_process'
 try {
   let usedCores = 0
   const cores = core.getInput('cores') || cpus().length
+  console.log(core.getInput('cmds'))
 
-  let commands = core
-    .getInput('cmds')
-    .split('\n')
-    .map(s =>
-      s
-        .split(' ')
-        .map(s => s.trim())
-        .flat()
-    )
+  // let commands = core
+  //   .getInput('cmds')
+  //   .split('\n')
+  //   .map(s =>
+  //     s
+  //       .split(' ')
+  //       .map(s => s.trim())
+  //       .flat()
+  //   )
 
-  const executingCommands = []
+  // const executingCommands = []
 
-  while (commands.length > 0) {
-    if (usedCores < cores) {
-      const [cmd, ...args] = commands.shift()
-      console.info('Executing cmd', cmd, args)
-      const promise = new Promise((resolve, reject) => {
-        const p = spawn(cmd, args)
-        p.on('message', msg => console.info(msg.toString()))
-        p.on('exit', c => {
-          if (c === 0) {
-            usedCores--
-            resolve('complete')
-          } else {
-            reject()
-          }
-        })
-      })
-      executingCommands.push(promise)
-      usedCores++
-    } else {
-      await new Promise(res => setTimeout(res, 1000))
-    }
-  }
+  // while (commands.length > 0) {
+  //   if (usedCores < cores) {
+  //     const [cmd, ...args] = commands.shift()
+  //     console.info('Executing cmd', cmd, args)
+  //     const promise = new Promise((resolve, reject) => {
+  //       const p = spawn(cmd, args)
+  //       p.on('message', msg => console.info(msg.toString()))
+  //       p.on('exit', c => {
+  //         if (c === 0) {
+  //           usedCores--
+  //           resolve('complete')
+  //         } else {
+  //           reject()
+  //         }
+  //       })
+  //     })
+  //     executingCommands.push(promise)
+  //     usedCores++
+  //   } else {
+  //     await new Promise(res => setTimeout(res, 1000))
+  //   }
+  // }
 
-  await Promise.all(executingCommands)
+  // await Promise.all(executingCommands)
 
   const time = new Date().toTimeString()
   core.setOutput('time', time)
