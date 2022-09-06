8c7393b338a218ec61dcab8eec72f36a6f01ca8a
Zach Smith <zachsmith.sa@gmail.com>
2022-09-06 11:01:47 +0200

    WIP - testing local parallel-cmds action

--- .github/actions/parallel-cmds/index.js
+++ .github/actions/parallel-cmds/index.js
@@ -2,14 +2,30 @@
 import core from '@actions/core'
 import github from '@actions/github'
 import {cpus} from 'os'
+import {spawn } from 'child_process'
 
 try {
   const cores = core.getInput('cores') || cpus().length
   const commands = core.getInput('cmds')
 
   console.log('cores', cores)
-  console.log(typeof commands, commands)
-  console.log(commands.split('\n'))
+
+  const results = await Promise.all(commands.split('\n').map(s =>  new Promise((resolve, reject) => {
+      const cmd = s.trim()
+      console.info('Executing cmd', cmd)
+      const p = spawn(cmd)
+      p.on('message', msg => console.info(msg.toString()))
+      p.on('exit', code => {
+        if (code === 0) {
+          resolve()
+        } else {
+          reject()
+        }
+      })
+    })
+  ))
+
+  console.log('All done!')
 
   const time = new Date().toTimeString()
   core.setOutput('time', time)
