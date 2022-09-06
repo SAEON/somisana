a16b5be88cf40169cd28f206bfe6a2ff61f40efb
Zach Smith <zachsmith.sa@gmail.com>
2022-09-06 11:29:21 +0200

    WIP - testing local parallel-cmds action

--- .github/actions/parallel-cmds/index.js
+++ .github/actions/parallel-cmds/index.js
@@ -39,7 +39,7 @@ try {
       executingCommands.push(promise)
       usedCores++
     } else {
-      await new Promise(res => setTimeout(res, 1000))
+      await new Promise(res => setTimeout(res, 10000))
     }
   }
 
