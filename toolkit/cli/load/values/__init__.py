815948e7af7d2f57b3ccb7c26eed3143b132dd44
Zach Smith <zachsmith.sa@gmail.com>
2022-09-16 14:47:01 +0200

    fix typo

--- toolkit/cli/load/values/__init__.py
+++ toolkit/cli/load/values/__init__.py
@@ -8,13 +8,8 @@ def upsert(runid, start_time, depths, datetimes):
     depth_levels = [*range(int(start_depth), int(end_depth) + 1, 1)]
     for depth_level in depth_levels:
         load(depth_level, runid, start_time, datetimes)
-    
+
     with pool().connection() as client:
         client.execute(
-            """
-            update public.runs
-            set successful = true
-            where runid = %s;
-            """,
-            (runid, )
+            """update public.runs set successful = true where id = %s;""", (runid,)
         )
