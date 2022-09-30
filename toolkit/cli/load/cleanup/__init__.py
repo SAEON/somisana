0ef0cb062f4473b4885e1879d84ea4c363e900c1
Zach Smith <zachsmith.sa@gmail.com>
2022-09-30 14:36:15 +0200

    Added analyze statements for tables in toolkit, to refresh postgres statistics

--- toolkit/cli/load/cleanup/__init__.py
+++ toolkit/cli/load/cleanup/__init__.py
@@ -10,18 +10,24 @@ def run(start_time, model):
     )
 
     with pool().connection() as client:
-        client.execute(
-            """
-            delete from public.rasters r
-            where rid in (
-                select
-                  x.rasterid
-                from
-                  raster_xref_run x
-                  join runs r on r.id = x.runid
-                  join models m on m.id = r.modelid
-                where
-                  m.name = %s);
-            """,
-            (model,),
-        )
+      # Remove raster files
+      client.execute(
+          """
+          delete from public.rasters r
+          where rid in (
+              select
+                x.rasterid
+              from
+                raster_xref_run x
+                join runs r on r.id = x.runid
+                join models m on m.id = r.modelid
+              where
+                m.name = %s);
+          """,
+          (model,),
+      )
+
+      # Re-analyze the tables
+      client.execute('analyze public.runs;')
+      client.execute('analyze public.coordinates;')
+      client.execute('analyze public.values;')
