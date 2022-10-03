08d5adcdc0cde844fee32c603a715fbfb0f9a7aa
Zach Smith <zachsmith.sa@gmail.com>
2022-10-03 06:38:22 +0200

    Updated runner temp files cleanup to be part of model run workflow instead of defined in crontab separately

--- web/server/graphql/resolvers/queries/data/index.js
+++ web/server/graphql/resolvers/queries/data/index.js
@@ -5,20 +5,32 @@ export default async (_, { timeStep, runId, depth }, ctx) => {
 
   ctx.compress = true
   try {
-    const res = await client.query({
-      text: `select
-      st_x(st_transform( c.coord, 4326)) x,
-      st_y(st_transform( c.coord, 4326)) y,
-      v.interpolated_temperature::float
-      from somisana_interpolate_values(
-        target_depth => $1,
-        runid => $2,
-        time_step => $3
-      ) v
-      join coordinates c on c.id = v.coordinateid;`,
-      values: [depth, runId, timeStep],
-      rowMode: 'array',
-    })
+    const res =
+      depth === 0
+        ? await client.query({
+            text: `
+    
+    `,
+            values: [runId, timeStep],
+            rowMode: 'array',
+          })
+        : await client.query({
+            text: `
+        select
+          st_x(st_transform( c.coord, 4326)) x,
+          st_y(st_transform( c.coord, 4326)) y,
+          v.interpolated_temperature::float
+        from
+          somisana_interpolate_values(
+            target_depth => $1,
+            runid => $2,
+            time_step => $3
+          ) v
+        join
+          coordinates c on c.id = v.coordinateid;`,
+            values: [depth, runId, timeStep],
+            rowMode: 'array',
+          })
 
     return {
       id: `${timeStep}${runId}${depth}`,
