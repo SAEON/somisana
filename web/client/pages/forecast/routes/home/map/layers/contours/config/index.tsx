97e5e363c366d278c8d5403c5bee638e0f06e7ff
Zach Smith <zachsmith.sa@gmail.com>
2022-10-06 13:47:07 +0200

    WIP adding scale config controls

--- web/client/pages/forecast/routes/home/map/layers/contours/config/index.tsx
+++ web/client/pages/forecast/routes/home/map/layers/contours/config/index.tsx
@@ -6,6 +6,7 @@ import DialogTitle from '@mui/material/DialogTitle'
 import DialogContent from '@mui/material/DialogContent'
 import Typography from '@mui/material/Typography'
 import { Cog as CogIcon } from '../../../../../../../../components/icons'
+import TextField from '@mui/material/TextField'
 
 export default ({ scaleMin, scaleMax, setScaleMin, setScaleMax }) => {
   const [open, setOpen] = useState(false)
@@ -26,6 +27,32 @@ export default ({ scaleMin, scaleMax, setScaleMin, setScaleMax }) => {
       <Dialog open={open} onClose={() => setOpen(false)}>
         <DialogTitle title={title}>{title}</DialogTitle>
         <DialogContent dividers>
+          <TextField
+            fullWidth
+            variant="standard"
+            label="Min"
+            type="number"
+            margin="normal"
+            size="small"
+            helperText="Minimum value to use for the color range"
+            step=".01"
+            value={scaleMin}
+            placeholder="Auto"
+            onChange={({ target: { value } }) => setScaleMin(value)}
+          />
+          <TextField
+            fullWidth
+            variant="standard"
+            label="Max"
+            type="number"
+            margin="normal"
+            size="small"
+            helperText="Max value to use for the color range"
+            step=".01"
+            value={scaleMax}
+            placeholder="Auto"
+            onChange={({ target: { value } }) => setScaleMax(value)}
+          />
           <Typography gutterBottom>
             Not implemented yet. Should be possible to define constant or auto min/max values,
             specify manual or auto intervals, save settings to browser storage so that settings are
