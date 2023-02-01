18df8510d7e64d5fc817bdcf20c20ba08d50ff81
Zach Smith <zachsmith.sa@gmail.com>
2023-02-01 11:59:48 +0200

    Updated web deps

--- web/server/graphql/resolvers/queries/locales/index.js
+++ web/server/graphql/resolvers/queries/locales/index.js
@@ -4,7 +4,7 @@ export default async (self, { id = undefined, languages }, ctx) => {
   const { findLocales } = ctx.mongo.dataFinders
 
   const query = {}
-  if (id) query._id = ObjectId(id)
+  if (id) query._id = new ObjectId(id)
   else if (languages) query.language = { $in: languages }
 
   return await findLocales(query)
