44f71f736207aaee63768bdb29c2aa4ee1d0178a
Zach Smith <zachsmith.sa@gmail.com>
2023-02-03 16:10:50 +0200

    Starting looking at adding the marine heat tracker

---
+++ env.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+export "PATH=$PATH:$(pwd)/web/bin"
+chmod +x $(pwd)/web/bin/s
--- web/cli/modules/integrations/index.js
+++ web/cli/modules/integrations/index.js
@@ -1,8 +1,27 @@
-import { describe } from '@saeon/cli-tools'
+import { describe, withFlags } from '@saeon/cli-tools'
+import lacce from '../../../integrations/lacce/index.js'
+import marineHeatWaves from '../../../integrations/marine-heat-waves/index.js'
 
 export default describe(
   {
-    stub: () => console.log('working'),
+    lacce: describe(
+      withFlags(async args => {
+        await lacce()
+      }, {}),
+      {
+        title: 'LACCE',
+        description: "Identify the Location of the Agulhas Current's Core and Edges (LACCE)",
+      }
+    ),
+    mhw: describe(
+      withFlags(async args => {
+        await marineHeatWaves()
+      }, {}),
+      {
+        title: 'Marine Heat Waves',
+        description: 'Identify the spatial extent marine heat waves',
+      }
+    ),
   },
   { title: 'Integration scripts', description: 'Integration CLI tooling' }
 )
