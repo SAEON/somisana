e23a0a255c4f7049c60351c39337c2962b2dc4e3
Zach Smith <zachsmith.sa@gmail.com>
2023-02-08 08:23:55 +0200

    .

--- env.sh
+++ env.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+# Web CLI
 export "PATH=$PATH:$(pwd)/web/bin"
 chmod +x $(pwd)/web/bin/s
+
+# Toolkit CLI
+export "PATH=$PATH:$(pwd)/toolkit/bin"
+chmod +x $(pwd)/toolkit/bin/somisana
