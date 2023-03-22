c1813be10c672b1530e67e544b3288ef3929613c
Nkululeko <nkululekolerato@gmail.com>
2023-03-22 12:56:24 +0200

    Changes made on the about page and a picture added

--- web/client/modules/about/_content.ts
+++ web/client/modules/about/_content.ts
@@ -1,18 +1,19 @@
 export default `
-# Another heading
-Some content
+# WOES Model preview:
+The model offers a wide range of applications such as 
+- A 5 day running hind cast 
+- A 5 day running forecast
+- A now cast
+- Historical archived datasets
+- Analysis across rho levels
+- Seasonal Means
 
-## Sub heading
-Sub heading content
+## Seasonal Mean
+![WOES Model Seasonal SST](/path/to/WOES Model Seasonal SSTs.png "WOES Model Seasonal SST")
 
 # Our vision
-Facilitate the local development and sustainability of an operational ocean current forecast system for the South African exclusive economic zone and to do so in a transformative fashion
+Facilitate the local development and sustainability of an 
+operational ocean current forecast system for the South African exclusive economic zone and to do so in a transformative fashion
 
-# Another heading
-This is another heading
-
-- bullet point:
-## New Heading:
-Lets improve this.
 
 `
