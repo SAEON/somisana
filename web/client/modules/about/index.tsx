ba84e35c20cde7831ce5df0d6a73e9daa61d9893
Zach Smith <zachsmith.sa@gmail.com>
2023-03-22 13:22:53 +0200

    Update about page layout

--- web/client/modules/about/index.tsx
+++ web/client/modules/about/index.tsx
@@ -3,17 +3,26 @@ import content from './_content'
 import Markdown from 'react-markdown'
 import Div from '../../components/div'
 import Img from '../../components/img'
+import Paper from '@mui/material/Paper'
+import { alpha } from '@mui/material/styles'
 
 const About = () => (
   <Div sx={{ my: theme => theme.spacing(2), flex: 1 }}>
     <Container sx={{ margin: 'auto' }}>
-      <Markdown
-        components={{
-          img: props => <Img width="100%" {...props} />,
+      <Paper
+        sx={{
+          p: 4,
+          backgroundColor: theme => alpha(theme.palette.common.white, 0.9),
         }}
       >
-        {content}
-      </Markdown>
+        <Markdown
+          components={{
+            img: props => <Img width="100%" {...props} />,
+          }}
+        >
+          {content}
+        </Markdown>
+      </Paper>
     </Container>
   </Div>
 )
