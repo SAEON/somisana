9c67d962c7623e9cb1169676ad77a326f717d50c
Zach Smith <zachsmith.sa@gmail.com>
2023-05-15 15:23:06 +0200

    Refactored web app to use both d3 contouring libraries for now, as the tricontour is not working

--- web/server/graphql/resolvers/queries/data/index.js
+++ web/server/graphql/resolvers/queries/data/index.js
@@ -6,20 +6,26 @@ export default async (_, { timeStep, runId, depth }, ctx) => {
     const res = await client.query({
       text: `
         select
-          coordinateid,
-          x,
-          y,
-          temperature,
-          salinity,
-          u,
-          v,
-          depth
+          v.coordinateid,
+          v.x,
+          v.y,
+          v.temperature,
+          v.salinity,
+          v.u,
+          v.v,
+          v.depth,
+          st_x (c.pixel) px,
+          st_y (c.pixel) py
         from
-        public.interpolated_values
+          public.interpolated_values v
+          right join coordinates c on c.id = v.coordinateid
         where
           runid = $1
           and time_step = $2
-          and depth = $3;`,
+          and depth = $3
+        order by
+          py desc,
+          px asc;`,
       values: [runId, timeStep, depth],
       rowMode: 'array',
     })
