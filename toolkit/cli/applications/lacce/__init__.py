9666c235479163df9244f1274b768ef3ea465d5f
Zach Smith <zachsmith.sa@gmail.com>
2023-06-08 15:21:00 +0200

    fix for broken toolkit

--- toolkit/cli/applications/lacce/__init__.py
+++ toolkit/cli/applications/lacce/__init__.py
@@ -1,4 +1,4 @@
-from cli.applications.lacce.download_lacce import download_lacce
-from cli.applications.lacce.detect_lacce import detect_lacce
+# from cli.applications.lacce.download_lacce import download_lacce
+# from cli.applications.lacce.detect_lacce import detect_lacce
 
 __all__ = ["download_lacce", "detect_lacce"]
