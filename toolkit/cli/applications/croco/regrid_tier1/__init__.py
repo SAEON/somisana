d0b68f5bab2b73f86316edb9a0bd62f766ae46d4
Zach Smith <zachsmith.sa@gmail.com>
2023-06-19 12:17:17 +0200

    Fixed missing values in model runs, and refactored usage logging code to be more succinct (useful for data portal as well)

--- toolkit/cli/applications/croco/regrid_tier1/__init__.py
+++ toolkit/cli/applications/croco/regrid_tier1/__init__.py
@@ -6,10 +6,11 @@ from datetime import timedelta, datetime
 import cli.applications.croco.postprocess as post
 import subprocess
 
-# We are hard coding the reference date here as all our CROCO output are 
+# We are hard coding the reference date here as all our CROCO output are
 # represented in seconds from 1 Jan 2000). This is of course not generally true
 # for all CROCO files
 REFERENCE_DATE = datetime(2000, 1, 1, 0, 0, 0)
+
 
 def regrid_tier1(args):
     id = args.id
@@ -25,30 +26,40 @@ def regrid_tier1(args):
 
     # Ensure the directory for the specified output exists
     os.makedirs(os.path.dirname(output), exist_ok=True)
-        
-    time_steps = post.get_time(input,REFERENCE_DATE)
+
+    time_steps = post.get_time(input, REFERENCE_DATE)
 
     # Get the run_date from the CROCO output NetCDF file
-    run_date = time_steps[int(len(time_steps)/2)-1].strftime("%Y%m%d") # TODO: rather make this an input - what if in future the hindcast and forecast durations aren't equal
+    run_date = time_steps[int(len(time_steps) / 2) - 1].strftime(
+        "%Y%m%d"
+    )  # TODO: rather make this an input - what if in future the hindcast and forecast durations aren't equal
     log("CONFIG::run_date", run_date)
-    
+
     log("Extracting the model output variables we need")
     with xr.open_dataset(input) as ds:
-        lon_rho=ds.lon_rho.values
-        lat_rho=ds.lat_rho.values
-        s_rho=ds.s_rho.values
-    h=post.get_var(input,grid,'h')
-    temp=post.get_var(input,grid,'temp')
-    salt=post.get_var(input,grid,'salt')
-    ssh=post.get_var(input,grid,'zeta')
-    
+        eta_rho = ds.eta_rho  # y index-based grid position
+        xi_rho = ds.xi_rho  # x index-based grid position
+        lon_rho = ds.lon_rho
+        lat_rho = ds.lat_rho
+        s_rho = ds.s_rho
+        h_ = ds.h
+        temp_ = ds.temp
+        salt_ = ds.salt
+        ssh_ = ds.zeta
+
+    # These have clever masking
+    h = post.get_var(input, grid, "h")
+    temp = post.get_var(input, grid, "temp")
+    salt = post.get_var(input, grid, "salt")
+    ssh = post.get_var(input, grid, "zeta")
+
     log("Regridding and rotating u/v")
-    u,v=post.get_uv(input,grid)
-    
+    u, v = post.get_uv(input, grid)
+
     # get the depth levels of the sigma layers
     log("Computing depth of sigma levels")
-    depth=post.get_depths(input,grid)
-    
+    depth = post.get_depths(input, grid)
+
     # Create new xarray dataset with selected variables
     log("Generating dataset")
     data_out = xr.Dataset(
@@ -58,32 +69,12 @@ def regrid_tier1(args):
             "run_date": run_date,
         },
         data_vars={
-            "zeta": xr.Variable(
-                ["time", "eta_rho", "xi_rho"],
-                ssh,
-                {
-                    "long_name": "averaged free-surface",
-                    "units": "meter",
-                    "standard_name": "sea_surface_height",                 
-                },                
-            ),
+            "zeta": xr.Variable(["time", "eta_rho", "xi_rho"], ssh, ssh_.attrs),
             "temp": xr.Variable(
-                ["time", "s_rho", "eta_rho", "xi_rho"],
-                temp,
-                {
-                    "long_name": "averaged potential temperature",
-                    "units": "Celsius",
-                    "standard_name": "sea_water_potential_temperature",                  
-                },
+                ["time", "s_rho", "eta_rho", "xi_rho"], temp, temp_.attrs
             ),
             "salt": xr.Variable(
-                ["time", "s_rho", "eta_rho", "xi_rho"],
-                salt,
-                {
-                    "long_name": "averaged salinity",
-                    "units": "PSU",
-                    "standard_name": "sea_water_salinity",                  
-                },
+                ["time", "s_rho", "eta_rho", "xi_rho"], salt, salt_.attrs
             ),
             "u": xr.Variable(
                 ["time", "s_rho", "eta_rho", "xi_rho"],
@@ -112,38 +103,22 @@ def regrid_tier1(args):
                     "positive": "up",
                 },
             ),
-            "h": xr.Variable(
-                ["eta_rho", "xi_rho"],
-                h,
-                {
-                    "long_name": "bathymetry at RHO-points",
-                    "units": "meter",
-                    "standard_name": "model_sea_floor_depth_below_geoid",
-                },
-            ),
+            "h": xr.Variable(["eta_rho", "xi_rho"], h, h_.attrs),
         },
         coords={
+            "eta_rho": xr.Variable(["eta_rho"], eta_rho.values, eta_rho.attrs),
+            "xi_rho": xr.Variable(["xi_rho"], xi_rho.values, xi_rho.attrs),
             "lon_rho": xr.Variable(
                 ["eta_rho", "xi_rho"],
-                lon_rho,
-                {
-                    "long_name": "longitude of RHO-points",
-                    "units": "degree_east" ,
-                    "standard_name": "longitude",
-                },
-            ),            
+                lon_rho.values,
+                lon_rho.attrs,
+            ),
             "lat_rho": xr.Variable(
                 ["eta_rho", "xi_rho"],
-                lat_rho,
-                {
-                    "long_name": "latitude of RHO-points",
-                    "units": "degree_west" ,
-                    "standard_name": "latitude",
-                },
+                lat_rho.values,
+                lat_rho.attrs,
             ),
-            "s_rho": xr.Variable(
-                ["s_rho"], s_rho, {"description": "Depth levels (grid levels)"}
-            ),
+            "s_rho": xr.Variable(["s_rho"], s_rho.values, s_rho.attrs),
             "time": xr.Variable(
                 ["time"],
                 time_steps,
@@ -164,7 +139,6 @@ def regrid_tier1(args):
         "lat_rho": {"dtype": "float32"},
         "s_rho": {"dtype": "float32"},
         "time": {"dtype": "i4"},
-        
     }
 
     log("Writing NetCDF file")
--- web/server/mongo/_logger.js
+++ web/server/mongo/_logger.js
@@ -80,43 +80,60 @@ const BATCH_SIZE = 1000
 export default collections => {
   return class LogBatcher {
     constructor() {
-      this._queries = []
+      this._queue = []
     }
 
-    async load(...queries) {
-      this._queries.push(...(await Promise.all(queries)))
+    async load(...logs) {
+      const resolvedQueries = await Promise.all(logs)
+      this._queue.push(...resolvedQueries)
+      this.processQueue()
+    }
 
+    async processQueue() {
+      if (!this._queue.length) {
+        return
+      }
+
+      /**
+       * Pushing this to the end of the event loop
+       * is probably unnecessary, but can potentially
+       * reduce the number of database trips
+       */
       setImmediate(async () => {
-        if (!this._queries.length) {
-          return
-        }
-
-        const batch = this._queries.slice(0, BATCH_SIZE)
-        this._queries = this._queries.slice(BATCH_SIZE)
-
-        try {
-          const { Logs } = await collections
-          const { insertedCount } = await Logs.insertMany(batch)
-          console.info('Client events logged', insertedCount)
-        } catch (error) {
-          if (error instanceof MongoClient.MongoError && error.name === 'BulkWriteError') {
-            const validationError = error.result.writeErrors[0].err
-            console.error(
-              'Error logging client events to Mongo. (validation error)',
-              validationError,
-              JSON.stringify(batch, null, 2)
-            )
-          } else {
-            console.error(
-              'Error logging client events to Mongo (ignore this unless very frequent).',
-              error,
-              JSON.stringify(batch, null, 2)
-            )
-          }
-        } finally {
-          this.load()
-        }
+        const batch = this._queue.slice(0, BATCH_SIZE)
+        this._queue = this._queue.slice(BATCH_SIZE)
+        await this.insertBatch(batch)
       })
+    }
+
+    async insertBatch(batch) {
+      try {
+        const { Logs } = await collections
+        const { insertedCount } = await Logs.insertMany(batch)
+        console.info('Client events logged', insertedCount)
+      } catch (error) {
+        this.handleError(error, batch)
+      } finally {
+        this.processQueue()
+      }
+    }
+
+    handleError(error, batch) {
+      if (error instanceof MongoClient.MongoError && error.name === 'BulkWriteError') {
+        const validationError = error.result.writeErrors[0].err
+        console.error(
+          'Error logging client events to Mongo. (validation error)',
+          validationError,
+          JSON.stringify(batch, null, 2)
+        )
+      } else {
+        console.error(
+          'Error logging client events to Mongo (ignore this unless very frequent).',
+          error,
+          JSON.stringify(batch, null, 2)
+        )
+      }
     }
   }
 }
+
