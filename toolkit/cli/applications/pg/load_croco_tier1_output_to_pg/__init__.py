d8435ab341fe8b90060ff0ebe9ecfb3f0e557668
Zach Smith <zachsmith.sa@gmail.com>
2023-06-20 15:55:13 +0200

    [PG Visualizations] Still bug fixing

--- toolkit/cli/applications/pg/load_croco_tier1_output_to_pg/__init__.py
+++ toolkit/cli/applications/pg/load_croco_tier1_output_to_pg/__init__.py
@@ -7,9 +7,6 @@ from config import PG_DB, PG_PORT, PG_HOST, PG_PASSWORD, PG_USERNAME
 from lib.log import log
 from datetime import datetime
 from lib.open_files import open_files
-from cli.applications.pg.load_croco_tier1_output_to_pg.upsert_model_info import (
-    upsert_model_info,
-)
 from cli.applications.pg.load_croco_tier1_output_to_pg.upsert_rasters import (
     upsert_rasters,
 )
@@ -20,12 +17,11 @@ from cli.applications.pg.load_croco_tier1_output_to_pg.upsert_model_run import (
 models_file = os.path.abspath("models.json")
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
-merge_details_file = os.path.join(current_dir, "upsert_model_info/upsert-details.sql")
 merge_coordinates_file = os.path.join(
-    current_dir, "upsert_model_info/upsert-coordinates.sql"
+    current_dir, "upsert-coordinates.sql"
 )
 update_geospatial_fields_file = os.path.join(
-    current_dir, "upsert_model_info/update-geospatial-fields.sql"
+    current_dir, "update-geospatial-fields.sql"
 )
 
 
@@ -53,13 +49,11 @@ async def run(args):
     # Open SQL and configuration files
     with open_files(
         models_file,
-        merge_details_file,
         merge_coordinates_file,
         update_geospatial_fields_file,
     ) as fds:
         (
             models_f,
-            merge_details_f,
             merge_coordinates_f,
             update_geospatial_fields_f,
         ) = fds
@@ -79,48 +73,78 @@ async def run(args):
                 )
                 exit()
 
-            # Upsert into public.runs
-            log("Starting transaction")
             async with pool.acquire() as conn:
-                async with conn.transaction():
-                    q1 = await conn.prepare(
-                        """
-                        merge into public.runs t
-                        using (
-                            select
-                                $1::date run_date,
-                                ( select id from models where name = $2 ) modelid
-                        ) s on
-                            s.run_date = t.run_date
-                            and s.modelid = t.modelid
-                        when
-                            not matched then
-                                insert (run_date, modelid)
-                                values (s.run_date, s.modelid);
-                        """
-                    )
-                    log(f" => {q1}")
-                    await q1.fetch(run_date, id)
+                # Ensure that model is registered
+                q1 = await conn.prepare(
+                    """
+                    merge into public.models t
+                    using (
+                    select
+                        $1 name, $2 title, $3 description, $4 creator, $5 creator_contact_email, $6 type, $7::int grid_width, $8::int grid_height, $9::int sigma_levels, $10::float min_x, $11::float max_x, $12::float min_y, $13::float max_y) s on s.name = t.name
+                    when not matched then
+                    insert (name, title, description, creator, creator_contact_email, type, grid_width, grid_height, sigma_levels, min_x, max_x, min_y, max_y)
+                        values (s.name, s.title, s.description, s.creator, s.creator_contact_email, s.type, s.grid_width, s.grid_height, s.sigma_levels, s.min_x, s.max_x, s.min_y, s.max_y)
+                        when matched then
+                        update set
+                            title = s.title, description = s.description, creator = s.creator, creator_contact_email = s.creator_contact_email, type = s.type, grid_width = s.grid_width, grid_height = s.grid_height, sigma_levels = s.sigma_levels, min_x = s.min_x, max_x = s.max_x, min_y = s.min_y, max_y = s.max_y
+                    """
+                )
+                await q1.fetch(
+                    id,
+                    model["title"],
+                    model["description"],
+                    model["creator"],
+                    model["creator_contact_email"],
+                    model["type"],
+                    model["grid_width"],
+                    model["grid_height"],
+                    model["sigma_levels"],
+                    model["min_x"],
+                    model["max_x"],
+                    model["min_y"],
+                    model["max_y"],
+                )
+                log(f"Upserted {id} info")
 
-                    # Get the run ID
-                    q2 = await conn.prepare(
-                        """
+                # Upsert into public.runs
+            
+                q1 = await conn.prepare(
+                    """
+                    merge into public.runs t
+                    using (
                         select
-                            r.id
-                        from
-                            runs r
-                        join
-                            models m on m.id = r.modelid 
-                        where
-                            r.run_date = $1
-                            and m.name = $2
-                        """
-                    )
-                    log(f" => {q2}")
-                    r = await q2.fetch(run_date, id)
-                    runid = r[0]["id"]
-                    log(f"  => Run ID {runid} cached")
-                
+                            $1::date run_date,
+                            ( select id from models where name = $2 ) modelid
+                    ) s on
+                        s.run_date = t.run_date
+                        and s.modelid = t.modelid
+                    when
+                        not matched then
+                            insert (run_date, modelid)
+                            values (s.run_date, s.modelid);
+                    """
+                )
+                log(f" => {q1}")
+                await q1.fetch(run_date, id)
+
+                # Get the run ID
+                q2 = await conn.prepare(
+                    """
+                    select
+                        r.id
+                    from
+                        runs r
+                    join
+                        models m on m.id = r.modelid 
+                    where
+                        r.run_date = $1
+                        and m.name = $2
+                    """
+                )
+                log(f" => {q2}")
+                r = await q2.fetch(run_date, id)
+                runid = r[0]["id"]
+                log(f"  => Run ID {runid} cached")
 
                 # Upsert rasters
                 variables = list(ds.keys())
@@ -135,19 +159,15 @@ async def run(args):
                             f"No PostGIS configuration found for raster {raster} (defined in models.yml). Skipping raster load"
                         )
 
-                    # Upsert model information
-                    await upsert_model_info(
-                        conn=conn,
-                        id=id,
-                        model=model,
-                        merge_details=merge_details_f.read(),
-                        merge_coordinates=merge_coordinates_f.read(),
-                        update_geospatial_fields=update_geospatial_fields_f.read(),
-                    )
-                log("Transaction complete")
+                # Upsert model information
+                q2 = await conn.prepare(merge_coordinates_f.read())
+                await q2.fetch(id)
+                log(f"Upserted {id} coordinates")
+                q3 = await conn.prepare(update_geospatial_fields_f.read())
+                await q3.fetch(id)
+                log(f"Upserted {id} geospatial info")
 
             # Upsert model run
-            # This can't be in a single transaction as it is split across many connections for parallelization
             await upsert_model_run(
                 runid=runid,
                 pool=pool,
