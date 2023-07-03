878a37caef380ef44d134e134a1fb2558598951d
Zach Smith <zachsmith.sa@gmail.com>
2023-07-03 12:03:50 +0200

    Removed features.somisana.ac.za service, since it's not used

--- web/client/src/modules/config/env/index.js
+++ web/client/src/modules/config/env/index.js
@@ -2,11 +2,11 @@ export const NODE_ENV = process.env.NODE_ENV
 export const HOSTNAME = process.env.REACT_APP_HOSTNAME || 'http://localhost:3001'
 export const API_ADDRESS = process.env.REACT_APP_API_ADDRESS || 'http://localhost:3000'
 export const TILESERV_BASE_URL = process.env.REACT_APP_TILESERV_BASE_URL
-export const FEATURESERV_BASE_URL = process.env.REACT_APP_FEATURESERV_BASE_URL
 export const ESRI_API_KEY = process.env.REACT_APP_ESRI_API_KEY
+export const TECHNICAL_CONTACT =
+  process.env.REACT_APP_TECHNICAL_CONTACT || 'Missing configuration value [TECHNICAL_CONTACT]'
+
 export const ORIGIN = HOSTNAME
 export const ESRI_BASEMAP = 'arcgis-oceans'
 export const API_HTTP = `${API_ADDRESS}/http`
 export const API_GQL = `${API_ADDRESS}/graphql`
-export const TECHNICAL_CONTACT =
-  process.env.REACT_APP_TECHNICAL_CONTACT || 'Missing configuration value [TECHNICAL_CONTACT]'
