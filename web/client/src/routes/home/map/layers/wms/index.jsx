a13308a333ef555cda5f8e50d3c23c44228744ea
Zach Smith <zachsmith.sa@gmail.com>
2023-07-21 21:55:07 +0200

    WIP on map

--- web/client/src/routes/home/map/layers/wms/index.jsx
+++ web/client/src/routes/home/map/layers/wms/index.jsx
@@ -11,9 +11,9 @@ const Render = ({ map }) => {
     map.addSource('wms', {
       type: 'raster',
       tiles: [
-        'https://thredds.somisana.ac.za/thredds/wms/data/somisana/sw-cape/5-day-forecast/202307/20230721-hourly-avg-t3.nc?REQUEST=GetMap&VERSION=1.3.0&LAYERS=temp&STYLES=default-scalar/default&PALETTE=default&BBOX={bbox-epsg-3857}&CRS=EPSG:3857&WIDTH=1024&HEIGHT=1024&FORMAT=image/png&transparent=true&elevation=200&time=2023-07-21T11:30:00.000Z',
+        'https://thredds.somisana.ac.za/thredds/wms/data/somisana/sw-cape/5-day-forecast/202307/20230721-hourly-avg-t3.nc?REQUEST=GetMap&VERSION=1.3.0&LAYERS=temp&STYLES=raster/default&&PALETTE=psu-viridis&BBOX={bbox-epsg-3857}&CRS=EPSG:3857&WIDTH=256&HEIGHT=256&FORMAT=image/png&transparent=true&elevation=0&time=2023-07-21T11:30:00.000Z&COLORSCALERANGE=0,30&NUMCOLORBANDS=200',
       ],
-      tileSize: 512,
+      tileSize: 256,
     })
     
 
