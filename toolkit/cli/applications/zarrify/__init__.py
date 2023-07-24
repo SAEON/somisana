5234731fd2182ddc5b9b44a6fdbb75aec2ae6c5d
Zach Smith <zachsmith.sa@gmail.com>
2023-07-24 22:21:01 +0200

    Updated zarr chunk config

--- toolkit/cli/applications/zarrify/__init__.py
+++ toolkit/cli/applications/zarrify/__init__.py
@@ -2,11 +2,15 @@ import xarray as xr
 from lib.log import log
 import os
 
+chunk_sizes = {"time": 24, "depth": 1, "latitude": -1, "longitude": -1}
+
+
 def zarrify(args):
-  input = os.path.abspath(args.input)
-  output = os.path.abspath(args.output)
-  log(f"Zarrifying {input} to {output}")
-  
-  ds = xr.open_dataset(input, chunks={})
-  ds.to_zarr(output, mode='w')
-  log(f"Complete!")
+    input = os.path.abspath(args.input)
+    output = os.path.abspath(args.output)
+    log(f"Zarrifying {input} to {output}")
+
+    ds = xr.open_dataset(input, chunks=chunk_sizes)
+
+    ds.to_zarr(output, mode="w", consolidated=True)
+    log(f"Complete!")
