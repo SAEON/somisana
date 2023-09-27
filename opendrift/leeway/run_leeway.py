a5bc658970ef0117f4eda98ef328a5d7f072e305
GilesFearon <gfearon11@gmail.com>
2023-09-27 17:09:13 +0200

    generalising reader code

--- opendrift/leeway/run_leeway.py
+++ opendrift/leeway/run_leeway.py
@@ -20,17 +20,7 @@ from opendrift.readers import reader_global_landmask
 import config_leeway as config
 import subprocess
 sys.path.append('/somisana/') # where the source code gets copied into the Docker image
-import plot_od,post_od
-
-def set_croco_time(reader_filename,date_ref):
-    # hacky solution to correct the time, as native croco files do not contain reference time
-    # code taken directly from reader_ROMS_native.py, just with updated time_units
-    ocean_time = reader_filename.Dataset.variables['time']
-    time_units = 'seconds since '+str(date_ref) 
-    reader_filename.times=num2date(ocean_time[:], time_units)
-    reader_filename.start_time = reader_filename.times[0]
-    reader_filename.end_time = reader_filename.times[-1]
-    return reader_filename
+import plot_od,post_od,pre_od
 
 def main():
 
@@ -55,57 +45,8 @@ def main():
     # -------------------------------------
     #
     lw = Leeway(loglevel=20)  # Set loglevel to 0 for debug information
-    #
-    # Readers
-    #
-    # increasing lw.max_speed from the default of 1.3 m/s due to warning messages - the Agulhas is fast
-    # we need to make sure we grab enough data from the readers at each time-step (read the docs for what this does)
-    # (tests indicate this only works if done before adding the readers)
-    lw.max_speed = 5 
-    # 
-    # Land
-    reader_landmask = reader_global_landmask.Reader()
-    lw.add_reader(reader_landmask)
-    #
-    # CROCO files covering the run
-    # 
-    # if you want to exclude currents for debugging:
-    #lw.set_config('environment:fallback:x_sea_water_velocity', 0)
-    #lw.set_config('environment:fallback:y_sea_water_velocity', 0)
-    #
-    # use the reader_ROMS_native reader
-    # in future, the loop below could be nested in a loop on levels so file extension would be nc.level
-    for croco_dir in config.croco_dirs:
-        reader_croco = reader_ROMS_native.Reader(croco_dir+'stable/'+config.croco_run_date+'/croco/forecast/hourly-avg-'+config.croco_run_date+'.nc')
-        reader_croco = set_croco_time(reader_croco,config.croco_ref_time)
-        lw.add_reader(reader_croco)
-    #
-    # MERCATOR currents
-    filename=config.eez_data_dir+config.croco_run_date+'/stable/mercator_'+config.croco_run_date+'.nc'
-    reader_mercator = reader_netCDF_CF_generic.Reader(filename)
-    lw.add_reader(reader_mercator)
-    #
-    # GFS wind
-    #
-    # if you want to exclude wind for debugging:
-    # lw.set_config('environment:fallback:x_wind', 0)
-    # lw.set_config('environment:fallback:y_wind', 0)
-    #
-    # for now we've got a hack solution to using the gfs winds - we're using the intermediate file written out
-    # by croco_tools  - it's the gfs data on it's native grid in a single nc files, rather than the hundreds
-    # of grb files which get downloaded and aren't so easy to use
-    #
-    filename='/tmp/algoa-bay-forecast/stable/'+config.croco_run_date+'/croco/forcing/GFS_'+config.croco_run_date+'.nc'
-    # I'm opening the file as an xarray dataset before passing to reader_netCDF_CF_generic
-    # because it had trouble with the time conversion inside the reader
-    # Doing it like this is a hack to get around this issue, as the time gets handled by xarray
-    # rather than by netCDF4's num2date function as done in the reader 
-    Dataset = xr.open_dataset(filename, decode_times=True) # decode_times=True is the default 
-    reader_gfs = reader_netCDF_CF_generic.Reader(Dataset,
-                            standard_name_mapping={'uwnd': 'x_wind',
-                                                   'vwnd': 'y_wind'},)    
-    lw.add_reader(reader_gfs)
-    
+    lw = pre_od.add_readers(lw,config)
+
     # -------------------
     # physical processes
     # -------------------
