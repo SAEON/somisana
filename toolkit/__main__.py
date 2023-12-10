88bdd0795d0494d2b40a1814b79cc307b8f06e59
Giles Fearon <gfearon11@gmail.com>
2023-12-10 12:35:09 +0200

    Revert "path fix?"
    This reverts commit e1832f0b604d8bc927e25b8e9c7f24f9d9a951c5.

--- toolkit/__main__.py
+++ toolkit/__main__.py
@@ -1,18 +1,6 @@
 # Add somisana's croco_tools dir for the 'croco' application in the toolkit
-#import sys
-#sys.path.append('../models/croco_tools-somisana')
-
-import os
 import sys
-
-# Get the absolute path of the current file
-current_file_path = os.path.abspath(__file__)
-
-# Get the parent directory of the current file
-parent_directory = os.path.dirname(os.path.dirname(current_file_path))
-
-# Add the parent directory to the Python path
-sys.path.append(parent_directory+'/models/croco_tools-somisana')
+sys.path.append('../models/croco_tools-somisana')
 
 from cli import cli
 
